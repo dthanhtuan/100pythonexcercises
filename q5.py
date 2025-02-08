75a754370862f7a2b0bd4b5fecddd20a344b3b2e
Tuan Dao <tuan.dao.fr@gmail.com>
2025-02-09 00:12:57 +0700

    Q5: The number is divisible by 3 and is in the range 50 to 100

---
+++ q5.py
@@ -0,0 +1,5 @@
+n = int(input("Enter a number: "))
+if (n % 3 == 0) and 50 <= n <= 100:
+    print("The number is divisible by 3 and is in the range 50 to 100")
+else:
+    print("The number is not divisible by 3 and is not in the range 50 to 100")
