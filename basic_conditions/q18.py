cf34d515268377dc54c4047b39938e29e15e7cb4
Tuan Dao <tuan.dao.fr@gmail.com>
2025-02-09 15:37:27 +0700

    Add Q18

---
+++ basic_conditions/q18.py
@@ -0,0 +1,9 @@
+a = float(input("Enter the number 1: "))
+b = float(input("Enter the number 2: "))
+if a != 0:
+    print("X = ", -b / a)
+else:
+    if b != 0:
+        print("No solution")
+    else:
+        print("Infinite solutions")
