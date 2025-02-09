b5003f3c28c40e9b7245c75c25952fbdca2b95ad
Tuan Dao <tuan.dao.fr@gmail.com>
2025-02-09 15:25:32 +0700

    Add Q16

--- basic_conditions/q16.py
+++ basic_conditions/q16.py
@@ -3,9 +3,9 @@ b = int(input("Enter the number 2: "))
 c = int(input("Enter the number 3: "))
 
 
-def is_triangle(a, b, c):
-    if all(value > 0 for value in [a, b, c]):
-        if a + b > c and a + c > b and b + c > a:
+def is_triangle(a1, b1, c1):
+    if all(value > 0 for value in [a1, b1, c1]):
+        if a1 + b1 > c1 and a1 + c1 > b1 and b1 + c1 > a1:
             print("The numbers can be sides of a triangle")
             return True
         else:
@@ -16,10 +16,32 @@ def is_triangle(a, b, c):
         return False
 
 
+def isoceles(a1, b1, c1):
+    if a1 == b1 or b1 == c1 or a1 == c1:
+        return True
+    return False
+
+
+def equilateral(a1, b1, c1):
+    if a1 == b1 == c1:
+        return True
+    return False
+
+
+def right_angled(a1, b1, c1):
+    if a1 * a1 == b1 * b1 + c1 * c1 or b1 * b1 == a1 * a1 + c1 * c1 or c1 * c1 == a1 * a1 + b1 * b1:
+        return True
+    return False
+
+
 if is_triangle(a, b, c):
-    if a == b == c:
+    if equilateral(a, b, c):
         print("The triangle is equilateral")
-    elif a == b or b == c or a == c:
+    elif isoceles(a, b, c):
         print("The triangle is isosceles")
+    elif right_angled(a, b, c):
+        print("The triangle is right-angled")
+        if isoceles(a, b, c):
+            print("The triangle is also isosceles")
     else:
         print("The triangle is scalene")
