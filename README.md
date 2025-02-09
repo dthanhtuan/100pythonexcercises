27f8f84953e64dea4195c1bc2da8cfabb324130d
Tuan Dao <tuan.dao.fr@gmail.com>
2025-02-09 15:01:22 +0700

    Add Q12

---
+++ README.md
@@ -0,0 +1,2 @@
+### 100 BÀI TẬP LẬP TRÌNH TỪ CƠ BẢN ĐẾN NÂNG CAO
+https://hocpython.org/100-bai-tap-lap-trinh-tu-co-ban-den-nang-cao/
---
+++ q12.py
@@ -0,0 +1,8 @@
+a = float(input("Enter the number 1: "))
+b = float(input("Enter the number 2: "))
+c = float(input("Enter the number 3: "))
+d = (a + b) ** c
+if 100 <= d <= 200:
+    print("True")
+else:
+    print("False")
