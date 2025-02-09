31b7dfef60798991703d16c9dfccf5d78bb653cc
Tuan Dao <tuan.dao.fr@gmail.com>
2025-02-09 15:15:03 +0700

    Add Q15

---
+++ basic_conditions/q15.py
@@ -0,0 +1,11 @@
+a = float(input("Enter the number 1: "))
+b = float(input("Enter the number 2: "))
+c = float(input("Enter the number 3: "))
+
+if all(value > 0 for value in [a, b, c]):
+    if a + b > c and a + c > b and b + c > a:
+        print("The numbers can be sides of a triangle")
+    else:
+        print("The numbers cannot be sides of a triangle")
+else:
+    print("All numbers are not positive")
