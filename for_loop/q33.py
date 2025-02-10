da2d9a52eeef4a6327657e6a0c6daa5f24334e03
Tuan Dao <tuan.dao.fr@gmail.com>
2025-02-10 23:00:32 +0700

    Add Q33

---
+++ for_loop/q33.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a number: "))
+i = 2
+not_a_prime = False
+while i <= a:
+    if i != a and a % i == 0:
+        not_a_prime = True
+    i += 1
+if not_a_prime:
+    print("Not a prime number")
+else:
+    print("Prime number")
