3d6f9943edc1df3e429ff6b4156bb015a1a38ce3
Tuan Dao <tuan.dao.fr@gmail.com>
2025-02-10 22:53:01 +0700

    Add Q32

--- for_loop/q32.py
+++ for_loop/q32.py
@@ -2,7 +2,11 @@ a = int(input("Enter a number: "))
 b = int(input("Enter a number: "))
 i = 1
 max = a if a > b else b
+max_factor = 0
 while i <= max:
     if a % i == 0 and b % i == 0:
         print(i)
+        if i > max_factor:
+            max_factor = i
     i += 1
+print("Max common factor:", max_factor)
