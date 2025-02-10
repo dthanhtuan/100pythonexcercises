457e2f0af26894becdd2e7f8aae70d6059788462
Tuan Dao <tuan.dao.fr@gmail.com>
2025-02-10 21:23:40 +0700

    Add Q24

---
+++ for_loop/q24.py
@@ -0,0 +1,9 @@
+print("Odd numbers between 1 and 100 using for loop:")
+for i in range(1, 100, 2):
+    print(i)
+
+print("Odd numbers between 1 and 100 using while loop:")
+i = 1
+while i < 100:
+    print(i)
+    i += 2
